feebadce0f620f795114e0a6e4d9810f77d8ef1d
toop <173619288+1toop@users.noreply.github.com>
2025-12-08 20:44:39 +0300

    Update uilib.lua

--- uilib.lua
+++ uilib.lua
@@ -2922,7 +2922,8 @@ function Library:_theme_selector()
 	local grid = colorThemesContainer:object("UIGridLayout", {
 		CellPadding = UDim2.fromOffset(10, 10),
 		CellSize = UDim2.fromOffset(102, 83),
-		VerticalAlignment = Enum.VerticalAlignment.Center
+		VerticalAlignment = Enum.VerticalAlignment.Center,
+		HorizontalAlignment = Enum.HorizontalAlignment.Center
 	})
 
 	colorThemesContainer:object("UIPadding", {
